d5b93062aeca1745bf4ebc6b1a1ac0d7317837ea
Kartik Rao <kr40.git@gmail.com>
2021-04-15 13:31:57 +0000

    Changed some styles

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,13 @@
 <main>
 	<div class="container">
 		<label class="lead text-primary" for="name">Enter your name: </label>
-		<input bind:value={name} placeholder="Your name" id="name" type="text" />
+		<input
+			class="text-secondary"
+			bind:value={name}
+			placeholder="Your name"
+			id="name"
+			type="text"
+		/>
 	</div>
 	<h1>Hello {name} !</h1>
 	<button class="btn" on:click={add}>Add</button>
@@ -16,7 +22,7 @@
 	<button class="btn" on:click={() => (count = 0)}>Reset</button>
 </main>
 <div class="container">
-	<p>This is current count: {count}</p>
+	<p class="text-secondary">This is current count: {count}</p>
 </div>
 
 <style>
@@ -30,6 +36,17 @@
 
 	.text-primary {
 		color: #ff3e00;
+		padding: 1em;
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 1.5em;
+		font-weight: 100;
+	}
+
+	.text-secondary {
+		font-size: 1em;
+		font-weight: 100;
+		font-family: Roboto;
 	}
 
 	main {
