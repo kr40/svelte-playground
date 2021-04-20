02572e2f26152c333dcdbbc519eacc0a25c60087
kr40 <kr40.git@gmail.com>
2021-04-20 18:11:04 +0530

    removed bulma, added water.css

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Svelte Playground
 
-This is a Svelte Playground to mess around in or to start a new project. It comes with `normalize.css`, `bulma.css`, `font-awesome 5` & `stylelint`.
+This is a Svelte Playground to mess around in or to start a new project. It comes with `normalize.css`, `water.css`, `font-awesome 5` & `stylelint`.
 
 **Assumes `prettier` is already configured.**
+
+`water.css` is a no-class `css` framework which just makes things look good out of the box. It comes with a automatic dark/light theme switching based on `prefer-color-scheme` media query, just use -
+
+`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">`.
+
+**Note: Remove the existing stylesheet used for `water.css` which I used to make the theme switcher if you wish to use the automatic theme.**
 
 ## Usage
 
--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,40 @@
 <script>
-	console.log('Hello From the Console');
+	function toggleTheme() {
+		// Obtains an array of all <link> elements.
+		// Select your element using indexing.
+
+		const theme = document.getElementsByTagName('link')[2];
+
+		// Change the value of href attribute to change the css sheet.
+
+		if (theme.getAttribute('href') == 'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css') {
+			theme.setAttribute('href', 'https://cdn.jsdelivr.net/npm/water.css@2/out/light.css');
+		} else {
+			theme.setAttribute('href', 'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css');
+		}
+	}
 </script>
 
-<main class="hero is-large">
-	<section class="hero-body has-text-centered">
-		<h1 class="title is-size-1 has-text-primary-dark ">Hello World</h1>
-		<p class="has-text-info-dark is-size-5">
-			This is a svelte template which comes with <strong class="has-text-danger-dark"
-				>normalize.css, bulma.css, font-awesome 5 & stylelint.</strong>
-			<br />
-			<strong class="has-text-danger-dark">
-				<span><i class="fas fa-info-circle" />&nbsp;</span>Assumes Prettier is already configured.</strong>
-		</p>
-	</section>
+<main class="container">
+	<button on:click={toggleTheme}>Switch Theme</button>
+	<h1><span><i class="fas fa-home" /></span>&nbsp;Svelte Playground</h1>
+	<p>
+		This is a svelte template which comes with <strong
+			>normalize.css, water.css, font-awesome 5 & stylelint.</strong>
+		<br /><br />
+
+		<strong>
+			<span><i class="fas fa-info-circle" /></span>&nbsp; Assumes Prettier is already configured.</strong>
+	</p>
 </main>
 
 <style>
-	main {
-		font-family: Overpass, monospace;
+	.container {
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
 	}
 </style>
