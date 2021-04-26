660f52a3d2909a6323235dbf6c40aaeffe001218
kr40 <kr40.git@gmail.com>
2021-04-26 14:54:44 +0530

    added eslint changed svelte version

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+module.exports = {
+	extends: ['eslint:recommended', 'google', 'prettier'],
+	env: {
+		browser: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+	plugins: ['svelte3'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3',
+			rules: {
+				// 'import/no-extraneous-dependencies': 'off',
+				// 'import/no-mutable-exports': 'off',
+				// 'import/first': 'off',
+			},
+		},
+	],
+	rules: {
+		// 'no-use-before-define': ['error', 'nofunc'],
+		// 'import/prefer-default-export': 'off',
+	},
+};
